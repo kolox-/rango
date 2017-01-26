af1804519ed5cd173cebaa23c79b96f97ccb89e4
Colin Butler <2031389b@student.gla.ac.uk>
2017-01-26 17:17:23 +0000

    [6.2] Added dynamic listing of categories to index.html

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,9 +7,15 @@ def index(request):
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
