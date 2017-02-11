b42dc73c1bfc7a0e255ae847731fe71ced3fb411
Colin Butler <2031389b@bo620-16u.dcs.gla.ac.uk>
2017-02-11 23:06:43 +0000

    [9.5] Added User support for images and websites

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -31,3 +32,20 @@ class Page(models.Model):
 
     def __str__(self):  # For Python 2, use __unicode__ too
         return self.title
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return something meaningful
+    # Remember to define __unicode__ too
+    def __str__(self):
+        return self.user.username
+
+    def __unicode_(self):
+        return self.user.username
