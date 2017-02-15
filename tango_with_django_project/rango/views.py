66e9bba339a6c11754fadddccd034d5256ad9f3c
kolox- <colinbutler1995@gmail.com>
2017-02-15 22:36:22 +0000

    [9.9] Added view/url for logging out

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
@@ -203,6 +203,15 @@ def user_login(request):
         # No context variables to pass to the template system (blank dict)
         return render(request, 'rango/login.html', {})
 
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now log them out
+    logout(request)
+    # Take the user back to the homepage
+    return HttpResponseRedirect(reverse('index'))
+    
+    
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
